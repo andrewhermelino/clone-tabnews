c85f299601e535f138a19be5510390a903137ac8
Andrew <andrew.contate@gmail.com>
2023-10-16 22:31:31 -0300

    Adicionado mensagem com cantada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import { useState, useRef, useEffect } from "react";
+
+const link = {
+  backgroundColor: "#D9D9D9",
+  padding: 10,
+  borderRadius: 6,
+  color: "black",
+  textDecoration: "none",
+};
+
 function Home() {
-  return <h1>Olá mundo 🌎😊</h1>;
+  return (
+    <main style={{ fontFamily: "Arial", textAlign: "center" }}>
+      <h1>Oieeee Gatouna!</h1>
+      <p style={{ paddingBottom: 15 }}>
+        Por acaso você é Európio, Telúrio, Amerício e Oxigênio? 😁
+      </p>
+      <a style={link} href="eutiamu">
+        Não, por que?
+      </a>
+    </main>
+  );
 }
 
 export default Home;
